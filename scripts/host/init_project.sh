3f2a30bc4821591cb180f24fb2529988261d6bcb
Alexander Paliarush <apaliarush@magento.com>
2015-12-31 13:32:39 +0200

    - Added PHP Storm configuration during project initialization

--- scripts/host/init_project.sh
+++ scripts/host/init_project.sh
@@ -3,10 +3,6 @@
 vagrant_dir=$(cd "$(dirname "$0")/../.."; pwd)
 magento_ce_dir="${vagrant_dir}/magento2ce"
 magento_ee_dir="${magento_ce_dir}/magento2ee"
-ce_repository_default="${vagrant_dir}/local.config/ce_repository_url.txt.dist"
-ce_repository_custom="${vagrant_dir}/local.config/ce_repository_url.txt"
-# By default EE repository is not specified and EE project cannot be checked out
-ee_repository_custom="${vagrant_dir}/local.config/ee_repository_url.txt"
 
 # Enable trace printing and exit on the first error
 set -ex
@@ -14,16 +10,12 @@ set -ex
 bash "${vagrant_dir}/scripts/host/check_requirements.sh"
 
 # Check out CE repository
-rm -f "${magento_ee_dir}/.gitkeep"
-rm -rf ${magento_ee_dir}
-if [ -f ${ce_repository_custom} ]; then
-    ce_repository_url=$(cat ${ce_repository_custom})
-else
-    ce_repository_url=$(cat ${ce_repository_default})
-fi
+ce_repository_url=$(sh "${vagrant_dir}/scripts/host/get_variable_value.sh" "ce_repository_url")
 git clone ${ce_repository_url} ${magento_ce_dir}
 
 # Check out EE repository
+# By default EE repository is not specified and EE project is not checked out
+ee_repository_custom="${vagrant_dir}/local.config/ee_repository_url.txt"
 if [ -f ${ee_repository_custom} ]; then
     ee_repository_url=$(cat ${ee_repository_custom})
     git clone ${ee_repository_url} ${magento_ee_dir}
@@ -31,7 +23,6 @@ else
     set +x
     echo "Note: URL to Magento EE repository may be specified in ${ee_repository_custom}, then it will be checked out automatically."
     set -x
-    mkdir ${magento_ee_dir}
 fi
 
 # Update Magento dependencies via Composer
@@ -41,3 +32,5 @@ bash "${vagrant_dir}/scripts/host/composer.sh" install
 # Create vagrant project
 cd ${vagrant_dir}
 vagrant up
+
+bash "${vagrant_dir}/scripts/host/configure_php_storm.sh"
