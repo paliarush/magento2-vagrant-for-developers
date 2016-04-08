6f55db2a0db01277983a8d27a49de3e1cfa82b5d
Olga Kopylova <okopylova@ebay.com>
2016-04-07 23:40:55 -0500

    Remove github token requirement
    - removed token from config
    - removed GitHub limitations section from Readme

--- scripts/host/check_requirements.sh
+++ scripts/host/check_requirements.sh
@@ -10,10 +10,3 @@ set -ex
 if ! ${php_executable} -v | grep -q 'Copyright' ; then
     bash "${vagrant_dir}/scripts/host/install_php.sh"
 fi
-
-if [ ! -f ${composer_auth_json} ]; then
-    set +x
-    echo "Please specify GitHub token in ${composer_auth_json} to bypass GitHub rate limits (see https://github.com/paliarush/magento2-vagrant-for-developers/tree/develop#github-limitations)"
-    exit 255
-    set -x
-fi
