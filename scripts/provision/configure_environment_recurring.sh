cb4890073fb3e1863b18732416961c2fcfa382dd
Arkadii Chyzhov <achyzhov@ebay.com>
2016-05-31 21:33:41 +0300

    -updated recurring script

--- scripts/provision/configure_environment_recurring.sh
+++ scripts/provision/configure_environment_recurring.sh
@@ -42,13 +42,15 @@ if [ -f ${vagrant_dir}/.idea/deployment.xml ]; then
 fi
 
 # Setup PHP
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-apt-get install -y php5.6 php-xdebug php5.6-xml php5.6-mcrypt php5.6-curl php5.6-cli php5.6-mysql php5.6-gd php5.6-intl php5.6-bcmath php5.6-mbstring php5.6-soap php5.6-zip libapache2-mod-php5.6
-echo '
-xdebug.max_nesting_level=200
-xdebug.remote_enable=1
-xdebug.remote_connect_back=1' >> /etc/php/5.6/mods-available/xdebug.ini
+if [ ! -d "/etc/php/5.6" ]; then
+    sudo add-apt-repository ppa:ondrej/php
+    sudo apt-get update
+    sudo apt-get install -y php5.6 php-xdebug php5.6-xml php5.6-mcrypt php5.6-curl php5.6-cli php5.6-mysql php5.6-gd php5.6-intl php5.6-bcmath php5.6-mbstring php5.6-soap php5.6-zip libapache2-mod-php5.6
+    echo '
+    xdebug.max_nesting_level=200
+    xdebug.remote_enable=1
+    xdebug.remote_connect_back=1' >> /etc/php/5.6/mods-available/xdebug.ini
+fi
 
 php_ini_paths=( /etc/php/7.0/cli/php.ini /etc/php/5.6/cli/php.ini )
 process_php_config ${php_ini_paths}
