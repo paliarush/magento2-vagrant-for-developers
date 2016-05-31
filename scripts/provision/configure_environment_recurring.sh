f2da2bc53c967e3dd6cfc5b878376159581c3adb
Arkadii Chyzhov <achyzhov@ebay.com>
2016-05-31 22:28:52 +0300

    -updated recurring script

--- scripts/provision/configure_environment_recurring.sh
+++ scripts/provision/configure_environment_recurring.sh
@@ -2,13 +2,16 @@
 
 function process_php_config () {
     php_ini_paths=$1
+
     for php_ini_path in "${php_ini_paths[@]}"
     do
-        echo "date.timezone = America/Chicago" >> ${php_ini_path}
-        sed -i "s|;include_path = \".:/usr/share/php\"|include_path = \".:/usr/share/php:${guest_magento_dir}/vendor/phpunit/phpunit\"|g" ${php_ini_path}
-        sed -i "s|display_errors = Off|display_errors = On|g" ${php_ini_path}
-        sed -i "s|display_startup_errors = Off|display_startup_errors = On|g" ${php_ini_path}
-        sed -i "s|error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = E_ALL|g" ${php_ini_path}
+        if [ -f ${php_ini_path} ]; then
+            echo "date.timezone = America/Chicago" >> ${php_ini_path}
+            sed -i "s|;include_path = \".:/usr/share/php\"|include_path = \".:/usr/share/php:${guest_magento_dir}/vendor/phpunit/phpunit\"|g" ${php_ini_path}
+            sed -i "s|display_errors = Off|display_errors = On|g" ${php_ini_path}
+            sed -i "s|display_startup_errors = Off|display_startup_errors = On|g" ${php_ini_path}
+            sed -i "s|error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = E_ALL|g" ${php_ini_path}
+        fi
     done
 }
 
@@ -56,7 +59,11 @@ php_ini_paths=( /etc/php/7.0/cli/php.ini /etc/php/5.6/cli/php.ini )
 process_php_config ${php_ini_paths}
 
 if [ ${use_php7} -eq 1 ]; then
-    update-alternatives --set php /usr/bin/php7.0 && a2dismod php5.6 && a2enmod php7.0
+    update-alternatives --set php /usr/bin/php7.0
+    if [ -d "/etc/php/5.6" ]; then
+        a2dismod php5.6
+    fi
+    a2enmod php7.0
 else
     if [ ! -d "/etc/php/5.6" ]; then
         init_php56
