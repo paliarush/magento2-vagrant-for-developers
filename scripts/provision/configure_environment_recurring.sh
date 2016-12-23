fa8fa279bbaf51f4856926c4300ccfb9e1e4ffe9
Alex Paliarush <apaliarush@magento.com>
2016-12-23 14:05:53 -0600

    Fixed problems after vagrant suspend #81

--- scripts/provision/configure_environment_recurring.sh
+++ scripts/provision/configure_environment_recurring.sh
@@ -50,20 +50,15 @@ incrementNestingLevel
 
 status "Removing configs from host in case of force stop of virtual machine before linking restored ones"
 cd "${vagrant_dir}/etc" && mv guest/.gitignore guest_gitignore.back && rm -rf guest && mkdir guest && mv guest_gitignore.back guest/.gitignore
-bash "${vagrant_dir}/scripts/guest/link_configs"
 
 status "Making sure configs are restored on system halt and during reboot"
+# Unlink here helps in case when Virtual Machine was suspended
+bash "${vagrant_dir}/scripts/guest/unlink_configs"
+bash "${vagrant_dir}/scripts/guest/link_configs"
+
 rm -f /etc/init.d/unlink-configs
 cp "${vagrant_dir}/scripts/guest/unlink_configs" /etc/init.d/unlink-configs
-if [[ ! -f /etc/rc0.d/K04-unlink-configs ]]; then
-    ln -s /etc/init.d/unlink-configs /etc/rc0.d/K04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc1.d/S04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc2.d/S04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc3.d/S04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc4.d/S04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc5.d/S04-unlink-configs
-    ln -s /etc/init.d/unlink-configs /etc/rc6.d/K04-unlink-configs
-fi
+update-rc.d unlink-configs defaults 04 2> >(log) > >(log)
 
 status "Upgrading existing environment"
 if [[ -f "${vagrant_dir}/.idea/deployment.xml" ]]; then
