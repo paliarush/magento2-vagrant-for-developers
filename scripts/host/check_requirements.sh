0c51228dbef9fc365e8cc4db58c8f7deb14c1f3f
Alex Paliarush <paliarus@adobe.com>
2019-01-11 10:49:56 -0600

    - Added scripts for automated NFS configuration

--- scripts/host/check_requirements.sh
+++ scripts/host/check_requirements.sh
@@ -7,10 +7,10 @@ source "${vagrant_dir}/scripts/functions.sh"
 status "Checking requirements"
 incrementNestingLevel
 
-php_executable="$(bash "${vagrant_dir}/scripts/host/get_path_to_php.sh")"
-
-if ! ${php_executable} -v 2> >(log) | grep -q 'Copyright' ; then
-    bash "${vagrant_dir}/scripts/host/install_php.sh"
+nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=501:20 -mask 255.0.0.0 -network 192.0.0.0"
+if [[ -z "$(grep "${nfs_exports_record}" /etc/exports)" ]]; then
+    error "NFS exports configuration required on the host. Please execute 'sudo ${vagrant_dir}/scripts/host/configure_nfs_exports.sh' first."
+    exit 1
 fi
 
 decrementNestingLevel
---
+++ scripts/host/configure_nfs_exports.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+vagrant_dir=$PWD
+
+source "${vagrant_dir}/scripts/functions.sh"
+resetNestingLevel
+current_script_name=`basename "$0"`
+initLogFile ${current_script_name}
+
+debug_vagrant_project="$(bash "${vagrant_dir}/scripts/get_config_value.sh" "debug_vagrant_project")"
+if [[ ${debug_vagrant_project} -eq 1 ]]; then
+    set -x
+fi
+
+nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=501:20 -mask 255.0.0.0 -network 192.0.0.0"
+if [[ -z "$(grep "${nfs_exports_record}" /etc/exports)" ]]; then
+    status "Updating /etc/exports to enable codebase sharing with containers via NFS"
+    echo "${nfs_exports_record}" | sudo tee -a "/etc/exports" 2> >(logError) > >(log)
+    # TODO: Implement NFS exports clean up on project removal to prevent NFS mounting errors
+else
+    status "NFS exports are properly configured and do not need to be updated"
+fi
+
+info "$(regular)See details in $(bold)${vagrant_dir}/log/${current_script_name}.log$(regular). For debug output set $(bold)debug:vagrant_project$(regular) to $(bold)1$(regular) in $(bold)etc/config.yaml$(regular)"
