ef845a324e0afaed2db371338585324b1db1d2b9
Alex Paliarush <paliarus@adobe.com>
2019-01-16 17:48:33 -0600

    - Parameterized nfs exports configuration script

--- scripts/host/check_requirements.sh
+++ scripts/host/check_requirements.sh
@@ -7,7 +7,7 @@ source "${vagrant_dir}/scripts/functions.sh"
 status "Checking requirements"
 incrementNestingLevel
 
-nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=501:20 -mask 255.0.0.0 -network 192.0.0.0"
+nfs_exports_record="\"${vagrant_dir}\" -alldirs -mapall=$(id -u):$(id -g) -mask 255.0.0.0 -network 192.0.0.0"
 if [[ -z "$(grep "${nfs_exports_record}" /etc/exports)" ]]; then
     warning "NFS exports configuration required on the host. Please execute 'sudo ${vagrant_dir}/scripts/host/configure_nfs_exports.sh' first."
     exit 1
